9fd3eb4aaf3b2fdb9d314df142691284bb2c1902
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-03-18 15:50:57 -0600

    fix path of cart page to be /api/orders

--- components/tables.js
+++ components/tables.js
@@ -1,37 +1,69 @@
-import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
+import React, { Component } from "react";
+import { StyleSheet, View, YellowBox } from "react-native";
+import {
+  Table,
+  TableWrapper,
+  Text,
+  Row,
+  Rows,
+  Col,
+  Cols,
+  Cell
+} from "react-native-table-component";
+import axios from "axios";
 
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: "#fff" },
+  head: { height: 40, backgroundColor: "#f1f8ff" },
+  text: { margin: 6 }
+});
 
-export default class Tables extends Component {
+class Tables extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      tableHead: ['Items', 'Quantity', 'Price', 'Delete'],
-      tableData: [
-        ['1', '2', '3', '4'],
-        ['a', 'b', 'c', 'd'],
-        ['1', '2', '3', '4'],
-        ['a', 'b', 'c', 'd']
-      ]
-    }
+      selectedProducts: [],
+      tableHead: ["Items", "UPC", "Recyclable"]
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`https://bugi-api.herokuapp.com/api/orders`)
+
+      .then((data) => {
+        console.log("dataaaaaa", data);
+        const selectedProducts = data.data.map((item, i) =>
+          Object.values(item, i)
+        );
+
+        this.setState({ selectedProducts: selectedProducts });
+        this.setState((prevState) => ({
+          ...prevState,
+          selectedProducts: [...prevState.selectedProducts, ...selectedProducts]
+        }));
+      })
+      .catch((err) => {
+        console.log(" catch here ", err);
+      });
   }
 
   render() {
-    const state = this.state;
+    const product = this.state.selectedProducts;
+    console.log("product", product);
+
     return (
       <View style={styles.container}>
-        <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
-          <Row data={state.tableHead} style={styles.head} textStyle={styles.text}/>
-          <Rows data={state.tableData} textStyle={styles.text}/>
+        <Table borderStyle={{ borderWidth: 2, borderColor: "#c8e1ff" }}>
+          <Row
+            data={this.state.tableHead}
+            style={styles.head}
+            textStyle={styles.text}
+          />
+          <Rows data={this.state.selectedProducts} textStyle={styles.text} />
         </Table>
       </View>
-    )
+    );
   }
 }
-
-const styles = StyleSheet.create({
-  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
-  head: { height: 40, backgroundColor: '#f1f8ff' },
-  text: { margin: 6 }
-});
+export default Tables;

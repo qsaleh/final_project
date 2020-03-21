ad333ec94abf289e3d15162552f8854fe0ce6b0c
Ahmed Yagoub <ahyagoub40@gmail.com>
2020-03-21 15:26:36 -0600

    poplulated the credit card info. created a token

--- components/card-info.js
+++ components/card-info.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { TextInput } from 'react-native-paper';
 import ButtonWithBackground from "./ButtonWithBackground";
+import Stripe from 'react-native-stripe-api';
 
 class Card extends Component {
   constructor(props) {
@@ -14,28 +15,53 @@ class Card extends Component {
     };
 
   }
+  confirmPayment() {
+    const apiKey = 'pk_test_Jxd3WCtPiiGGGQORl0AxKkN500Ta61gIx2';
+    const client = new Stripe(apiKey);
+    client.createToken(
+      '4242424242424242',
+      '09',
+      '18',
+      '111'
+    )
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((error) => {
+        console.log(error.message)
+      });
+  }
   render() {
     return (
       <View >
         <TextInput
           label='credit card number'
           placeholder={""}
+          value={'4242424242424242'}
           onChangeText={(number) => this.setState({ number })}
         />
         <TextInput
           label='exp month'
           placeholder={""}
+          value={'09'}
           onChangeText={(expMonth) => this.setState({ expMonth })}
         />
         <TextInput
           label='exp year'
           placeholder={""}
+          value={'18'}
           onChangeText={(expYear) => this.setState({ expYear })}
         />
         <TextInput
           label='cvc'
           placeholder={""}
+          value={'111'}
           onChangeText={(cvc) => this.setState({ cvc })}
+        />
+        <ButtonWithBackground
+          text="confirm payment"
+          color="#2C7873"
+          onPress={this.confirmPayment.bind(this)}
         />
       </View>
     );
--- components/payment.js
+++ components/payment.js
@@ -4,27 +4,11 @@ import ButtonWithBackground from "../components/ButtonWithBackground";
 import Card from "../components/card-info";
 import { navigation } from "@react-navigation/stack";
 import axios from 'axios';
+
 const Payment = ({ navigation: { navigate } }) => {
-  const confirmPayment = () => {
-    axios
-      .post(`https://bugi-api.herokuapp.com/api/orders/`)
-      .then((response) => {
-        navigate("Receipt");
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-  return [
-    <Card />,
-    <View style={styles.container}>
-      <ButtonWithBackground
-        text="confirm payment"
-        color="#2C7873"
-        onPress={confirmPayment}
-      />
-    </View>
-  ];
+  return (
+    <Card />
+  );
 };
 
 const styles = StyleSheet.create({

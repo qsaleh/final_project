0417f5c2acbf76ba2c9cf0a8b64e9094db69ace6
Ahmed Yagoub <ahyagoub40@gmail.com>
2020-03-22 18:18:55 -0600

    payment functionality is complete

--- components/payment.js
+++ components/payment.js
@@ -3,11 +3,33 @@ import { StyleSheet, View } from "react-native";
 import ButtonWithBackground from "../components/ButtonWithBackground";
 import Card from "../components/card-info";
 import { navigation } from "@react-navigation/stack";
-import axios from 'axios';
-
+import Stripe from 'react-native-stripe-api';
 const Payment = ({ navigation: { navigate } }) => {
+  const confirmPayment = () => {
+    const apiKey = 'pk_test_Jxd3WCtPiiGGGQORl0AxKkN500Ta61gIx2';
+    const client = new Stripe(apiKey);
+    client.createToken(
+      {
+        number: '4242424242424242',
+        exp_month: '09',
+        exp_year: '22',
+        cvc: '111'
+      })
+      .then(res => {
+        navigate("Receipt");
+        console.log(res);
+      })
+      .catch(err => console.log(err));
+  }
   return (
-    <Card />
+    <View>
+      <Card />
+      <ButtonWithBackground
+        text="confirm payment"
+        color="#2C7873"
+        onPress={confirmPayment}
+      />
+    </View>
   );
 };
 

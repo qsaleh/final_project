078907f33507de8b6c9fcb9351829251d26ea0cc
Qusai Saleh <qusaisaleh@Candices-MacBook-Pro.local>
2020-03-26 14:40:42 -0600

    login page

--- components/login.js
+++ components/login.js
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState} from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { TextInput } from 'react-native-paper';
 import ButtonWithBackground from "../components/ButtonWithBackground";
 
 const LogIn = ({ navigation: { navigate } }) => {
+  state = {
+    email: '',
+    password: ''
+ }
+ handleEmail = (text) => {
+    this.setState({ email: text })
+ }
+ handlePassword = (text) => {
+    this.setState({ password: text })
+ }
+ login = (email, pass) => {
+    alert('email: ' + email + ' password: ' + pass)
+ }
   return (
     <View style={styles.container}>
-      <TextInput
-        label='Email'
-      />
-      <TextInput
-        label='password'
-      />
-      <Text>Not a User?</Text>
+       <TextInput style = {styles.input}
+               underlineColorAndroid = "transparent"
+               placeholder = "Email"
+               placeholderTextColor = "#9a73ef"
+               autoCapitalize = "none"
+              //  onChangeText = {this.handleEmail}
+              />
+     <TextInput secureTextEntry={true} 
+            style = {styles.input}
+               underlineColorAndroid = "transparent"
+               placeholder = "Password"
+               placeholderTextColor = "#9a73ef"
+               autoCapitalize = "none"
+              //  onChangeText = {this.handlePassword}
+               />
+      
       <ButtonWithBackground
         style={styles.button}
         text="Log-In"
@@ -25,9 +47,14 @@ const LogIn = ({ navigation: { navigate } }) => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
+    paddingTop: 23
+ },
+ input: {
+    margin: 15,
+    height: 40,
+    borderColor: '#7a42f4',
+    borderWidth: 1
+ },
   button: {
     flex: 1,
     justifyContent: 'flex-end'

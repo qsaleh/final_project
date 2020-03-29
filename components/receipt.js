857558aeb7ac71cc5c93805b15b10cbe75a439f4
Ahmed Yagoub <ahyagoub40@gmail.com>
2020-03-29 17:41:42 -0600

    styled the carts and removed the table library

--- components/receipt.js
+++ components/receipt.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
-import { Table, Row, Rows } from "react-native-table-component";
 import { useGlobal } from '../lib/globals';
 
 const Receipt = () => {
@@ -10,25 +9,33 @@ const Receipt = () => {
     "quantity",
     "subtotal"
   ];
-  const products = cartItems;
-  const nestedData = products.map((product) => [
-    product.productName,
-    product.qty,
-    product.subTotal
-  ]);
-  const total = products.reduce(function (tot, product) {
+  const total = cartItems.reduce(function (tot, product) {
     return tot + product.subTotal;
   }, 0);
   return (
     <View style={styles.container}>
-      <Table borderStyle={{ borderWidth: 2, borderColor: "#c8e1ff" }}>
-        <Row
-          data={tableHead}
-          style={styles.head}
-          textStyle={styles.text}
-        />
-        <Rows data={nestedData} textStyle={styles.text} />
-      </Table>
+      <View style={[styles.tableView, { backgroundColor: "red" }]}>
+        {tableHead.map((headData, index) => (
+          <View key={index} textStyle={styles.text} style={styles.row}>
+            <Text>{headData}</Text>
+          </View>
+        ))
+        }
+      </View>
+      {cartItems.map((product, index) => (
+        <View key={index} style={styles.tableView}>
+          <View textStyle={styles.text} style={styles.row}>
+            <Text>{product.productName}</Text>
+          </View>
+          <View textStyle={styles.text} style={styles.row}>
+            <Text>{product.qty}</Text>
+          </View>
+          <View textStyle={styles.text} style={styles.row}>
+            <Text>{product.subTotal}</Text>
+          </View>
+        </View>
+      ))
+      }
       <View style={styles.total}>
         <Text>Total: ${total}</Text>
       </View>
@@ -41,5 +48,7 @@ const styles = StyleSheet.create({
   container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: "#fff" },
   head: { height: 40, backgroundColor: "#f1f8ff" },
   text: { margin: 6 },
-  total: { flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#fff" }
+  total: { flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#fff" },
+  tableView: { flexDirection: 'row', flex: 1, padding: 2, justifyContent: 'space-around', borderWidth: 1 },
+  row: { flex: 1, alignItems: 'center', justifyContent: 'center' }
 });
--- components/tables.js
+++ components/tables.js
@@ -1,13 +1,6 @@
 import React from "react";
-
-import { StyleSheet, View, Button } from "react-native";
-import { Table, Row, Rows } from "react-native-table-component";
+import { StyleSheet, View, Button, Text } from "react-native";
 import { useGlobal } from '../lib/globals';
-const styles = StyleSheet.create({
-  container: { flex: 1, padding: 16, paddingTop: 50, backgroundColor: "#fff" },
-  head: { height: 40, backgroundColor: "#f1f8ff" },
-  text: { margin: 6, justifyContent: "center", alignItems: "center" }
-});
 
 const Tables = () => {
   const { cartItems, DecreaseItem, IncrementItem } = useGlobal();
@@ -18,29 +11,46 @@ const Tables = () => {
     "price",
     "subtotal"
   ];
-  const products = cartItems;
-  const nestedData = products.map((product) => [
-    product.productName,
-    product.qty,
-    <View>
-      <Button title="+" onPress={() => IncrementItem(product)} />
-      <Button title="-" onPress={() => DecreaseItem(product)} />
-    </View>,
-    product.unitPrice,
-    product.subTotal
-  ]);
 
   return (
     <View style={styles.container}>
-      <Table borderStyle={{ borderWidth: 2, borderColor: "#c8e1ff" }}>
-        <Row
-          data={tableHead}
-          style={styles.head}
-          textStyle={styles.text}
-        />
-        <Rows data={nestedData} textStyle={styles.text} />
-      </Table>
+      <View style={[styles.tableView, { backgroundColor: "red" }]}>
+        {tableHead.map((headData, index) => (
+          <View key={index} textStyle={styles.text} style={styles.row}>
+            <Text>{headData}</Text>
+          </View>
+        ))
+        }
+      </View>
+      {cartItems.map((product, index) => (
+        <View key={index} style={styles.tableView}>
+          <View textStyle={styles.text} style={styles.row}>
+            <Text>{product.productName}</Text>
+          </View>
+          <View textStyle={styles.text} style={styles.row}>
+            <Text>{product.qty}</Text>
+          </View>
+          <View textStyle={styles.text} style={styles.row}>
+            <Button title="+" onPress={() => IncrementItem(product)} />
+            <Button title="-" onPress={() => DecreaseItem(product)} />
+          </View>
+          <View textStyle={styles.text} style={styles.row}>
+            <Text>{product.unitPrice}</Text>
+          </View>
+          <View textStyle={styles.text} style={styles.row}>
+            <Text>{product.subTotal}</Text>
+          </View>
+        </View>
+      ))
+      }
     </View>
   );
 }
 export default Tables;
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 16, backgroundColor: "#fff" },
+  text: { margin: 6, justifyContent: "center", alignItems: "center" },
+  tableView: { flexDirection: 'row', flex: 1, padding: 2, justifyContent: 'space-around', borderWidth: 1 },
+  row: { flex: 1, alignItems: 'center', justifyContent: 'center' }
+});
